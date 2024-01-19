b95b32cb4dc78f2249f5a432e287f380d09b1142
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2024-01-19 16:48:55 +0700

    feat: persist tasks and selected task data into local storage

--- src/store/task.ts
+++ src/store/task.ts
@@ -3,6 +3,7 @@ import { arrayMove } from '@dnd-kit/sortable';
 import { create } from 'zustand';
 
 import { Task } from '@/lib/types';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type TaskState = {
   tasks: Task[];
@@ -34,94 +35,110 @@ const initialState: TaskState = {
   selectedTask: null,
 };
 
-const taskStore = create<TaskState & TaskActions>()((set) => ({
-  ...initialState,
-  actions: {
-    addTask: (name, estimation, description) =>
-      set((state) => ({
-        tasks: [
-          ...state.tasks,
-          {
-            id: crypto.randomUUID(),
-            name,
-            estimation,
-            description,
-            isFinished: false,
-          },
-        ],
-      })),
-    selectTask: (task) =>
-      set((state) => ({
-        selectedTask: task.isFinished ? state.selectedTask : task,
-      })),
-    toggleFinishedTask: (id) =>
-      set((state) => {
-        let nextTasks = state.tasks;
-        nextTasks = nextTasks.map((task) => {
-          if (task.id === id) {
+const taskStore = create<TaskState & TaskActions>()(
+  persist(
+    (set) => ({
+      ...initialState,
+      actions: {
+        addTask: (name, estimation, description) =>
+          set((state) => ({
+            tasks: [
+              ...state.tasks,
+              {
+                id: crypto.randomUUID(),
+                name,
+                estimation,
+                description,
+                isFinished: false,
+              },
+            ],
+          })),
+        selectTask: (task) =>
+          set((state) => ({
+            selectedTask: task.isFinished ? state.selectedTask : task,
+          })),
+        toggleFinishedTask: (id) =>
+          set((state) => {
+            let nextTasks = state.tasks;
+            nextTasks = nextTasks.map((task) => {
+              if (task.id === id) {
+                return {
+                  ...task,
+                  isFinished: !task.isFinished,
+                };
+              }
+
+              return task;
+            });
+
+            const taskToPush = nextTasks.find((task) => task.id)!;
+            if (taskToPush.isFinished) {
+              nextTasks = [
+                ...nextTasks.filter((task) => task.id !== taskToPush.id),
+                taskToPush,
+              ];
+            }
+
+            let selectedTaskIndex: number | null = null;
+            nextTasks.every((task, index) => {
+              if (!task.isFinished) {
+                selectedTaskIndex = index;
+                return false;
+              }
+
+              return true;
+            });
+
             return {
-              ...task,
-              isFinished: !task.isFinished,
+              tasks: nextTasks,
+              selectedTask:
+                selectedTaskIndex !== null
+                  ? nextTasks[selectedTaskIndex]
+                  : null,
             };
-          }
+          }),
+        moveTasks: (id, destinationID) =>
+          set((state) => {
+            const taskIndex = state.tasks.findIndex((task) => task.id === id);
+            const destinationTaskIndex = state.tasks.findIndex(
+              (task) => task.id === destinationID
+            );
 
-          return task;
-        });
+            return {
+              tasks: arrayMove(state.tasks, taskIndex, destinationTaskIndex),
+            };
+          }),
+        deleteFinishedTasks: () =>
+          set((state) => ({
+            tasks: state.tasks.filter((task) => !task.isFinished),
+          })),
+        deleteAllTasks: () => set({ tasks: initialState.tasks }),
+        deleteTask: (id) =>
+          set((state) => ({
+            tasks: state.tasks.filter((task) => task.id !== id),
+          })),
+        editTask: (taskToEdit) =>
+          set((state) => ({
+            tasks: state.tasks.map((task) => {
+              if (task.id === taskToEdit.id) {
+                return taskToEdit;
+              }
 
-        const taskToPush = nextTasks.find((task) => task.id)!;
-        if (taskToPush.isFinished) {
-          nextTasks = [
-            ...nextTasks.filter((task) => task.id !== taskToPush.id),
-            taskToPush,
-          ];
-        }
-
-        let selectedTaskIndex: number | null = null;
-        nextTasks.every((task, index) => {
-          if (!task.isFinished) {
-            selectedTaskIndex = index;
-            return false;
-          }
-
-          return true;
-        });
-
-        return {
-          tasks: nextTasks,
-          selectedTask:
-            selectedTaskIndex !== null ? nextTasks[selectedTaskIndex] : null,
-        };
+              return task;
+            }),
+          })),
+      },
+    }),
+    {
+      name: 'task-storage',
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        tasks: state.tasks,
+        selectedTask: state.selectedTask,
       }),
-    moveTasks: (id, destinationID) =>
-      set((state) => {
-        const taskIndex = state.tasks.findIndex((task) => task.id === id);
-        const destinationTaskIndex = state.tasks.findIndex(
-          (task) => task.id === destinationID
-        );
-
-        return {
-          tasks: arrayMove(state.tasks, taskIndex, destinationTaskIndex),
-        };
-      }),
-    deleteFinishedTasks: () =>
-      set((state) => ({
-        tasks: state.tasks.filter((task) => !task.isFinished),
-      })),
-    deleteAllTasks: () => set({ tasks: initialState.tasks }),
-    deleteTask: (id) =>
-      set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) })),
-    editTask: (taskToEdit) =>
-      set((state) => ({
-        tasks: state.tasks.map((task) => {
-          if (task.id === taskToEdit.id) {
-            return taskToEdit;
-          }
-
-          return task;
-        }),
-      })),
-  },
-}));
+    }
+  )
+);
 
 export const useTasks = () => taskStore((state) => state.tasks);
 export const useSelectedTask = () => taskStore((state) => state.selectedTask);
